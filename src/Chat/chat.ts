df6bf87100d5d3d2c6dee3c6831c76bca16af06e
Your Name <you@example.com>
2021-02-23 10:54:42 -0300

    Alterações e testes no arquivo CHAT

--- src/Chat/chat.ts
+++ src/Chat/chat.ts
@@ -3,9 +3,14 @@ import { Socket, Server } from 'socket.io'
 
 export function Chat(io: Server, socket: Socket) {
 
+    //Vai receber a msg e depois emitir aos ouvintes
     const sendMessage = (msg: any) => {
         console.log(socket.id)
         io.emit('chat message', msg);
+    }
+
+    const hello = () => {
+        socket.broadcast.emit('hello', socket.id)
     }
 
     const disconnect = () => {
@@ -14,5 +19,6 @@ export function Chat(io: Server, socket: Socket) {
 
     socket.on("chat message", sendMessage);
     socket.on("disconnect", disconnect);;
+    socket.on("hello", hello);;
 
 }
